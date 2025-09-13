da7e7cdb4bdd347ea5eddbc17551b52b4235f1d7
Md.Rohan_Jabed <rohanjabed001@gmail.com>
2025-09-13 21:58:46 +0600

    Added Backend folder

---
+++ Backend/src/main/java/com/example/BrightAid/BrightAidApplication.java
@@ -0,0 +1,13 @@
+package com.example.BrightAid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightAidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrightAidApplication.class, args);
+	}
+
+}
